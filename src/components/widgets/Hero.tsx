650c31f853246f329e97c58fc899f42f727f78f6
unknown <wasim_iqbal@msn.com>
2023-04-30 15:06:05 +0500

    landing page

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -1,6 +1,6 @@
-import Wrapper from "@/components/shared/Wrapper"
-import Heroposter from "@/assets/Images/hero-poster.jpg"
-import Image from "next/image"
+import Wrapper from "@/components/shared/Wrapper";
+import Heroposter from "@/assets/Images/hero-poster.jpg";
+import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -13,23 +13,28 @@ const Hero = () => {
               Presidential Initiative for Artificial Intelligence &
               Computing(PIAIC)
             </h4>
-            <h1 className="text-5xl font-semibold mt-2 ">Certified Web 3.0 and Metaverse Developer</h1>
-            <p className="mt-4 text-slate-600  text-lg ">
-              In this brand-new type of curriculum, students will learn how to
-              make money and boost exports in the classroom and will begin doing
-              so within six months of the program beginning. It resembles a
-              cross between a corporate venture and an educational project.
+            <h1 className="text-5xl font-semibold mt-2 ">
+              Certified Web 3.0 and Metaverse Developer
+            </h1>
+            <p className="mt-4 text-slate-600  text-lg text-justify mr-4">
+              One and Quarter Years Panaverse DAO Earn as you Learn Program
+              Getting Ready for the Next Generation of the Internet
+              Consolidating Web 3.0, Metaverse, Artificial Intelligence (AI),
+              Cloud, Edge, Ambient Computing/IoT, Network Automation, and
+              Bioinformatics Technologies
             </p>
-            <button className="mt-6 text-bold h-10 px-5 text-white transition-colors duration-150 bg-teal-600	 rounded-full focus:shadow-outline hover:bg-teal-800">Learn More</button>
+            <button className="mt-6 text-bold h-10 px-5 text-white transition-colors duration-150 bg-teal-600	 rounded-full focus:shadow-outline hover:bg-teal-800">
+              Learn More
+            </button>
           </div>
           {/* {Right side} */}
-            <div className="flex-1 px-1">
+          <div className="flex-1 px-1">
             <Image src={Heroposter} alt="Hero Poster" />
-            </div>          
+          </div>
         </div>
       </Wrapper>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
