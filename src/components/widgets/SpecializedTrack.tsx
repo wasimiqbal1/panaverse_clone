636b12f4ecaf93044cfbec1705d5b4dd4fb14632
unknown <wasim_iqbal@msn.com>
2023-05-13 15:48:17 +0500

    Add Components

--- src/components/widgets/SpecializedTrack.tsx
+++ src/components/widgets/SpecializedTrack.tsx
@@ -11,7 +11,7 @@ const SpecializedTrack = () => {
           <h2 className="text-4xl font-semibold text-gray-900 mt-3 ">
             Specialized Tracks:
           </h2>
-          <p className="mt-6 text-slate-600 mr-5 text-lg text-justify ">
+          <p className="mt-6 text-slate-600 mr-5 text-lg text-justify max-w-screen-sm ">
             After completing the first three quarters the participants will
             select one or more specializations consisting of two courses each:
           </p>
@@ -19,11 +19,11 @@ const SpecializedTrack = () => {
 <div className="mt-10 flex gap-x-6 gap-y-8">
       {/* Content Left */}
       <div className="shadow-xl rounded-xl border border-slate-200 py-8 px-8 basis-8/12 ">
-  <h4 className="text-teal-700 text-lg ">Specialized Program</h4>
+  <h4 className="text-primary text-lg font-medium  ">Specialized Program</h4>
   <h3 className="text-2xl font-bold mt-1">Web 3.0 (Blockchain) and Metaverse Specialization</h3>
   <p className="text-lg text-slate-600 mt-2 ">This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.
 </p>
-<button className="text-teal-700 text-xl mt-4 underline flex items-center gap-x-1.5 ">Learn More <svg width="10" height="13" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+<button className="text-primary text-xl mt-4 underline flex items-center gap-x-1.5 ">Learn More <svg width="10" height="13" viewBox="0 0 8 11" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M1.00006 10.0368L6.06801 5.57117L1.09503 0.999994" stroke="#00616C" strokeWidth="2"/>
 </svg>
 </button>
