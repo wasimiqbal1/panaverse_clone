88151d5589d8adca8f139bb4107425212c2fb4c4
unknown <wasim_iqbal@msn.com>
2023-05-24 07:31:59 +0500

    Update Component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,29 +1,30 @@
-import Link from "next/link"
-import Logo from "/public/Logo.ico"
-import Image from "next/image"
-import Wrapper from "@/components/shared/Wrapper"
+import Link from "next/link";
+import Logo from "/public/Logo.ico";
+import Image from "next/image";
+import Wrapper from "@/components/shared/Wrapper";
 
 const Header = () => {
   return (
-    <Wrapper>
-      <header className="flex justify-between max-w-screen-xl mx-auto bg-white py-4 sticky top-0 items-center">
-        {/* Logo */}
-        <div>
-          {/* <h2 className="text-xl font-bold ">Panaverse Dao</h2> */}
-          <Image src={Logo} alt="Panaverse Dao Logo" height={70} />
+    <header className="static top-0 ">
+      <Wrapper>
+        <div className="flex justify-between bg-white sticky top-0 py-4 items-center">
+          {/* Logo */}
+          <div>
+            <Image src={Logo} alt="Panaverse Dao Logo" height={70} />
+          </div>
+          {/* Navigation Bar */}
+          <ul className="flex space-x-8 font-semibold ">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href="/Courses">Courses</Link>
+            </li>
+          </ul>
         </div>
-        {/* Navigation Bar */}
-        <ul className="flex space-x-8 font-semibold ">
-          <li>
-            <Link href={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link href="/Courses">Courses</Link>
-          </li>
-        </ul>
-      </header>
-    </Wrapper>
-  )
-}
+      </Wrapper>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
