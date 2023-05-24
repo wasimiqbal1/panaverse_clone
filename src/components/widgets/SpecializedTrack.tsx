551ec879b8a67edf0f3b6e3ad32818d0cfae7b15
unknown <wasim_iqbal@msn.com>
2023-05-24 07:19:14 +0500

    Update Component

--- src/components/widgets/SpecializedTrack.tsx
+++ src/components/widgets/SpecializedTrack.tsx
@@ -1,21 +1,20 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import Wrapper from "@/components/shared/Wrapper";
 import Quarterbox from "@/components/shared/QuarterBox";
 import Image from "next/image";
-//Images 
-import Web3Img  from "@/assets/images/web 3.webp";
-
+//Images
+import Web3Img from "@/assets/images/web 3.webp";
 
 export const programsData = [
   {
-    slug : "wmd",
+    slug: "wmd",
     headers: "Web 3.0 (Blockchain) and Metaverse Specialization",
     description:
       "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.",
     image: Web3Img,
-      quarters: [
+    quarters: [
       {
         discription:
           "W3-351: Developing Advanced Smart Contracts and Oracles and Planet-Scale Web 3.0 DApps",
@@ -31,13 +30,13 @@ export const programsData = [
     ],
   },
   {
-    slug : "ai",
+    slug: "ai",
     headers: "II.	Artificial Intelligence (AI) and Deep Learning Specialization",
     description:
       "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
-      image: Web3Img,
+    image: Web3Img,
 
-      quarters: [
+    quarters: [
       {
         discription:
           "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
@@ -45,22 +44,105 @@ export const programsData = [
         number: 4,
       },
       {
-        discription:
-          "AI-361: Deep Learning and MLOps",
+        discription: "AI-361: Deep Learning and MLOps",
         header: "Quarter V",
         number: 5,
       },
     ],
   },
-  
+  {
+    slug: "ai",
+    headers: "II.	Artificial Intelligence (AI) and Deep Learning Specialization",
+    description:
+      "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+    image: Web3Img,
+
+    quarters: [
+      {
+        discription:
+          "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
+        header: "Quarter IV",
+        number: 4,
+      },
+      {
+        discription: "AI-361: Deep Learning and MLOps",
+        header: "Quarter V",
+        number: 5,
+      },
+    ],
+  },
+  {
+    slug: "ai",
+    headers: "II.	Artificial Intelligence (AI) and Deep Learning Specialization",
+    description:
+      "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+    image: Web3Img,
+
+    quarters: [
+      {
+        discription:
+          "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
+        header: "Quarter IV",
+        number: 4,
+      },
+      {
+        discription: "AI-361: Deep Learning and MLOps",
+        header: "Quarter V",
+        number: 5,
+      },
+    ],
+  },
+  {
+    slug: "ai",
+    headers: "II.	Artificial Intelligence (AI) and Deep Learning Specialization",
+    description:
+      "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+    image: Web3Img,
+
+    quarters: [
+      {
+        discription:
+          "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
+        header: "Quarter IV",
+        number: 4,
+      },
+      {
+        discription: "AI-361: Deep Learning and MLOps",
+        header: "Quarter V",
+        number: 5,
+      },
+    ],
+  },
+  {
+    slug: "ai",
+    headers: "II.	Artificial Intelligence (AI) and Deep Learning Specialization",
+    description:
+      "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+    image: Web3Img,
+
+    quarters: [
+      {
+        discription:
+          "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
+        header: "Quarter IV",
+        number: 4,
+      },
+      {
+        discription: "AI-361: Deep Learning and MLOps",
+        header: "Quarter V",
+        number: 5,
+      },
+    ],
+  },
 ];
 
 const SpecializedTrack = () => {
+  const [selectedItem, setSelectedItem] = useState("wmd");
+  const selectedItemDate = programsData.find(
+    (item) => item.slug === selectedItem
+  );
 
-  const [selectedItem, setSelectedItem] = useState("wmd"); 
-  const selectedItemDate = programsData.find((item) => item.slug === selectedItem)
-
-  return (  
+  return (
     <section>
       <Wrapper>
         {/* Header */}
@@ -72,10 +154,10 @@ const SpecializedTrack = () => {
             After completing the first three quarters the participants will
             select one or more specializations consisting of two courses each:
           </p>
-         </div>
+        </div>
         <div className="mt-10 flex flex-col-reverse lg:flex-row gap-x-6 gap-y-8">
           {/* Content Left */}
-          <div className="shadow-xl rounded-xl border border-slate-200 py-8 px-8 basis-8/12 ">
+          <div className="shadow-xl sticky top-0 self-start rounded-xl border border-slate-200 py-8 px-8 basis-8/12 ">
             <h4 className="text-primary text-lg font-medium  ">
               Specialized Program
             </h4>
@@ -102,45 +184,43 @@ const SpecializedTrack = () => {
               </svg>
             </button>
             <div className="flex flex-col sm:flex-row gap-4 mt-8 ">
-            {/* my-20 flex flex-col md:flex-row gap-x-8 gap-y-6 */}
-
-            {
-              selectedItemDate?.quarters.map((item)=>(
+              {selectedItemDate?.quarters.map((item) => (
                 <Quarterbox
-                key={item.number}
-                discription={item.discription}
-                header={item.header}
-                number={item.number}
-                haveBorder={false}
-              />
-
-              ))
-            }
+                  key={item.number}
+                  discription={item.discription}
+                  header={item.header}
+                  number={item.number}
+                  haveBorder={false}
+                />
+              ))}
             </div>
           </div>
           {/* Content Right */}
-          <div className=" px-4 py-6 space-y-4 bg-slate-200 basis-4/12 flex-1 ">
-            {
-              programsData.map((item, i)=>(
-                <div onClick={()=>setSelectedItem(item.slug) } key={item.slug} className=" flex gap-x-4 items-center cursor-pointer">
-                <div className="flex-shrink-0 h-24 w-36">
-                 <Image src={item.image} alt={item.headers} className="h-24  object-cover"/> 
+          <div className=" px-4 py-6 space-y-4 bg-slate-200 basis-4/12 flex-1 rounded-xl border ">
+            {programsData.map((item, i) => (
+              <div
+                onClick={() => setSelectedItem(item.slug)}
+                key={item.slug}
+                className=" flex gap-x-4 items-center cursor-pointer "
+              >
+                <div className="flex-shrink-0 h-24 w-36 ">
+                  <Image
+                    src={item.image}
+                    alt={item.headers}
+                    className="h-24  object-cover"
+                  />
                 </div>
                 <div>
                   <h4 className="text-primary font-medium">
                     Specialized Program
                   </h4>
-                  <h3 className="text-xl font-semibold ">
-                    {item.headers}
-                  </h3>
+                  <h3 className="text-xl font-semibold ">{item.headers}</h3>
                 </div>
               </div>
-                 
-              ))
-            }
-
+            ))}
           </div>
         </div>
+        <div className="h-screen"></div>
       </Wrapper>
     </section>
   );
