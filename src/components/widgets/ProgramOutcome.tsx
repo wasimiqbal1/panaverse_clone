936c4dc987cf6ee653dc3b4c314e04c19bf283e8
unknown <wasim_iqbal@msn.com>
2023-05-26 07:16:19 +0500

    Add Components

--- src/components/widgets/ProgramOutcome.tsx
+++ src/components/widgets/ProgramOutcome.tsx
@@ -1,15 +1,23 @@
 // "use client";
 
+import Wrapper from "../shared/Wrapper";
+import outcome from "@/assets/Images/hero-poster.jpg";
 
-import Wrapper from '../shared/Wrapper'
 
 const ProgramOutcome = () => {
   return (
-<section>
-    <Wrapper>
-         
-    </Wrapper>
-</section>  )
-}
+    <section>
+      <Wrapper>
+        <div className="flex gap-8">
+            {/* Left Image */}
+            {/* <div> */}
+            {/* <Image src={outcome} alt="Hero Poster"/>
+            </div> */}
+            {/* Right Content */}
+        </div>
+      </Wrapper>
+    </section>
+  );
+};
 
-export default ProgramOutcome
+export default ProgramOutcome;
